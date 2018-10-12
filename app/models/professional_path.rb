b7d0a38c861ea93e9f4fff29be4fc367916521f3
Emily Tran <e99tran@gmail.com>
2018-10-11 21:41:27 -0400

    uncommented validating code in professional path model

--- app/models/professional_path.rb
+++ app/models/professional_path.rb
@@ -2,5 +2,5 @@ class ProfessionalPath < ApplicationRecord
 	
 	# validations: a resource is required to have
 	# the following fields present
-	# validates_presence_of :name, :title, :description, :contact
+	 validates_presence_of :name, :title, :description, :contact
 end
