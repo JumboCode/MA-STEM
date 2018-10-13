2a08e9f019df53759cbfbbbb376acd2967b3727c
NADLER <noah.adler@tufts.edu>
2018-10-13 15:16:04 -0400

    trying to fix r_type

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
+  resources :resources
+
   scope '/api' do
     resources :drinks
   end
