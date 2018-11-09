efb6cca9e86d977ecf236867a6c0ada5d3354a45
William Rusk <wgrusk@gmail.com>
2018-11-08 20:34:16 -0500

    added test for resource by location

--- spec/requests/resources_spec.rb
+++ spec/requests/resources_spec.rb
@@ -47,5 +47,33 @@ RSpec.describe 'Resources API', type: :request do
     end
   end
 
+  describe 'GET /resources/:location' do
+      before { get "/resources/#{resource_location}" }
+
+    context 'when the record exists' do
+      it 'returns the resource' do
+        expect(json).not_to be_empty
+          expect(json['location']).to eq(resource_location)
+      end
+
+      it 'returns status code 200' do
+          expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when the record does not exist' do
+      let(:resource_location) { "Poughkeepsie" }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+
+      it 'returns a not found message' do
+        expect(response.body).to match(/Couldn't find Resource/)
+      end
+    end
+  end
+
+
 
 end
