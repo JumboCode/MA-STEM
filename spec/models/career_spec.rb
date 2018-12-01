f23b5fd5cf62c2e25323a7cd20fe10f65b494de5
mohsr <mkrizvi98@gmail.com>
2018-12-01 15:12:54 -0500

    wrote validation tests

--- spec/models/career_spec.rb
+++ spec/models/career_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Career, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it { should validate_presence_of(:name) }
+	it { should validate_presence_of(:generic) }
+	it { should validate_presence_of(:description) }
+	it { should validate_presence_of(:interests) }
+	it { should validate_presence_of(:field) }
 end
