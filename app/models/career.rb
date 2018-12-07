fb151ae4d04aa8fc7d1892b7780fa8e5ae0b56f8
William Rusk <wgrusk@gmail.com>
2018-12-06 20:56:57 -0500

    Removed drinks from schema

--- app/models/career.rb
+++ app/models/career.rb
@@ -1,10 +1,10 @@
 class Career < ApplicationRecord
-	String     :name
+	String :name
 	String :strengths
 	String :generic
 	String :description
 	has_and_belongs_to_many :interests
 	belongs_to :field
 
-	#validates_presence_of :name, :strengths, :generic, :description, :career_id, :field_id
+	validates_presence_of :name, :strengths, :generic, :description, :career_id, :field_id
 end
--- spec/models/career_spec.rb
+++ spec/models/career_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Career, type: :model do
-# 	it { should validate_presence_of(:name) }
-# 	it { should validate_presence_of(:strengths) }
-# 	it { should validate_presence_of(:generic) }
-# 	it { should validate_presence_of(:description) }
-# 	# it { should validate_presence_of(:career_id) }
-# 	# it { should validate_presence_of(:field_id) }
+ 	it { should validate_presence_of(:name) }
+ 	it { should validate_presence_of(:strengths) }
+ 	it { should validate_presence_of(:generic) }
+ 	it { should validate_presence_of(:description) }
+ 	it { should validate_presence_of(:career_id) }
+ 	it { should validate_presence_of(:field_id) }
 end
