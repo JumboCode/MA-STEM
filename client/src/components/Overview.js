a480aff71f3454a9a0c1f4101b1238b38f80058a
Daniel Cetlin <daniel.cetlin@tufts.edu>
2019-02-06 19:42:21 -0500

    Styling tweaks to master

--- client/src/components/Overview.js
+++ client/src/components/Overview.js
@@ -5,23 +5,22 @@ import * as d3 from "d3";
 
 export class Overview extends Component {
   constructor () {
-    super()
-    this.state = {}
-    this.renderSVG = this.renderSVG.bind(this)
+    super();
+    this.state = {};
+    this.renderSVG = this.renderSVG.bind(this);
   }
 
   componentDidMount() {
     this.renderSVG();
-    fetch('http://mass-stem-backend.herokuapp.com/resources')
-      .then(response => response.json())
-      .then(data => this.setState({ data }));
+    // fetch('http://mass-stem-backend.herokuapp.com/resources')
+    //   .then(response => response.json())
+    //   .then(data => this.setState({ data }));
   }
 
   renderSVG () {
     var svg = d3.select("#bubbles").append("svg")
                 .attr("width", 1200)
                 .attr("height", 600);
-
 
     var positions = [{x: 500, y: 150,  r: 90, fill: '#F84908', name: "Our Vision"},
                      {x: 150, y: 90,   r: 80, fill: '#BE2113', name: "Design", name2: "Your", name3: "Future"},
@@ -30,7 +29,7 @@ export class Overview extends Component {
                      {x: 700, y: 360 , r: 85, fill: '#09C9F7', name: "College", name2: "Certificate", name3: "Programs"},
                      {x: 920, y: 400 , r: 85, fill: '#F03C66', name: "Summer", name2: "Programs"},
                      {x: 400, y: 400 , r: 85, fill: '#19F221', name: "Build", name2: "Your", name3: "Path"},
-                     ]      
+                     ];
                      
     var lines = [{ x1: positions[0].x, x2: positions[1].x,
                    y1: positions[0].y, y2: positions[1].y},
@@ -46,7 +45,7 @@ export class Overview extends Component {
                    y1: positions[5].y, y2: positions[3].y},
                   {x1: positions[0].x, x2: positions[6].x,
                    y1: positions[0].y, y2: positions[6].y}
-                  ]
+                  ];
 
     for (var i = 0; i < lines.length; i++) {
       svg.append("line")
@@ -55,8 +54,7 @@ export class Overview extends Component {
          .attr("y1", lines[i].y1)
          .attr("y2", lines[i].y2)
          .style("stroke", "white")          
-         .style("stroke-width", 2)
-
+         .style("stroke-width", 2);
     }
   
 
@@ -81,26 +79,14 @@ export class Overview extends Component {
     // Name 1 append
     var textGroup = circleGroup.append("text")
     .attr("x", d => {
-      
-      
       if ('name2' in d) {
-        if (d.name.length > 5) {
-        return d.x - 36
+        return (d.name.length > 5)? d.x - 36: d.x - 30;
       } else {
-        return d.x - 30
-      }
-      } else {
-        if (d.name.length > 10) {
-          return d.x - 73
-        } else {
-          return d.x - 60 
-        }}})
+        return (d.name.length > 10)? d.x - 73: d.x - 60;
+      }})
     .attr("y", d => {
-      if ('name2' in d && !('name3' in d)) {
-        return d.y - 10
-      }
-      else if ('name2' in d) {
-        return d.y - 25;
+      if ('name2' in d) {
+        return ('name3' in d)? d.y - 25 :d.y - 10;
       } else {
         return d.y + 10;
       }})
@@ -117,8 +103,7 @@ export class Overview extends Component {
     textGroup.append("tspan")
     .attr("x", (d, i) => {
       if ('name2' in d) {
-        var str = d.name2;
-        var strLen = str.length;
+        var strLen = d.name2.length;
         if (strLen > 7) {
           return d.x - 65
         } else if (strLen > 5) {
@@ -131,11 +116,7 @@ export class Overview extends Component {
       }
     })
     .attr("y", (d, i) => {
-      if ('name2' in d && !('name3' in d)) {
-        return d.y + 30
-      }
-      else {
-      return d.y + 15}
+      return ('name2' in d && !('name3' in d))? d.y + 30: d.y + 15;
     })
     .text(d => {return d.name2})
     .append("tspan");
@@ -146,16 +127,8 @@ export class Overview extends Component {
       {return "name3-" + i})
     .attr("x", (d, i) => {
       if ('name3' in d) {
-        var str = d.name3;
-        var strLen = str.length;
-        console.log(str + " " + strLen);
-        if (strLen > 6) {
-          return d.x - 55
-        } else if (strLen > 5) {
-          return d.x - 43
-        } else {
-          return d.x - 33
-        }
+        let strLen = d.name3.length;
+        return (strLen > 6)? d.x - 55: (strLen > 5)? d.x - 43: d.x - 33;
       } else {
         return d.x - 35
       }
@@ -170,7 +143,6 @@ export class Overview extends Component {
                            .attr("r", d => {return d.r})
                            .attr("fill", d => {return d.fill})
                            .attr("fill-opacity", "0");
-
 
      function mouseOver(obj) {
       obj.on('mouseover', function(d, i) {
@@ -187,9 +159,6 @@ export class Overview extends Component {
         .duration("200")
         .attr("transform", "translate(-5)")
         .style("font-size", "38");
-        // d3.select("#name1-" + i).transition()
-        // .duration("200")
-        // .attr("transform", "translate(200)");
       }).on('mouseout', function(d, i) {
         d3.select("#circle"+i).transition()
         .ease(d3.easeQuad)
@@ -207,7 +176,6 @@ export class Overview extends Component {
         .delay("100")
         .attr("transform", "translate(5)")
         .style("font-size", "33");
-        
       })
     }
     mouseOver(invisCircleGroup);
