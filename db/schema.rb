b20eec999dd9861e429820915abebdbbd0a22726
benlondon99 <32783398+benlondon99@users.noreply.github.com>
2019-02-24 18:36:00 -0500

    Removed Drinks and Ingredients Tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190224232709) do
+ActiveRecord::Schema.define(version: 20190224233418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,23 +44,6 @@ ActiveRecord::Schema.define(version: 20190224232709) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
-  end
-
-  create_table "drinks", force: :cascade do |t|
-    t.string   "title"
-    t.string   "description"
-    t.string   "steps"
-    t.string   "source"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "ingredients", force: :cascade do |t|
-    t.integer  "drink_id"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["drink_id"], name: "index_ingredients_on_drink_id", using: :btree
   end
 
   create_table "professionalpaths", force: :cascade do |t|
