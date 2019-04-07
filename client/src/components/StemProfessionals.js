d59f2bf7291fee20881341ede633c88baf952452
Nicole Kennedy <nicolekennedy@NLK-Macbook-Pro.local>
2019-04-07 18:18:47 -0400

    iteration update

--- client/src/components/StemProfessionals.js
+++ client/src/components/StemProfessionals.js
@@ -1,3 +1,4 @@
+import ReactDOM from 'react-dom';
 import React, { Component } from "react"; 
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { Button } from 'semantic-ui-react'
@@ -27,7 +28,9 @@ const blue  = {
 export class StemProfessionals extends Component {
     constructor () {
         super()
-        this.state = { tabIndex: 0 }
+        //this.state = { tabIndex: 0 }
+        this.state = { tabIndex: 0, 
+                first: true }
     }
 
     componentDidMount() {
@@ -58,29 +61,43 @@ export class StemProfessionals extends Component {
         var company
         var title
         var content
-        var currProfile = 0 
+        var currProfile = 0
 
-        // for (var i = 0; i < 2; i++) {
-        //     profile = allProfiles[i];
-        //     name = Object.values(profile)[0]
-        //     jobTitle = Object.values(profile)[1]
-        //     company = Object.values(profile)[2]
-        //     numTabs = Object.values(profile)[3].length
+        //we wanted to only initialize currProfile to 0 when its the first initial run through
+        if (this.state.first == true)
+        {
+            console.log("first state");
+            currProfile = 0;
+        }
 
-        //     for (var i = 0; i < numTabs; i++) {
-        //         title = Object.values(profile)[3][i].title
-        //         content = Object.values(profile)[3][i].content
-        //         tabTitle.push(<Tab style={ colors[i] }> {title} </Tab>);
-        //         tabContent.push(<TabPanel style={ colors[i] } className="tab-content"> {content} </TabPanel>);
-        //     }
-        // }
+        this.state.first = false;
+
+
+        console.log("here");
+        //creates profile
+        profile = allProfiles[currProfile];
+        name = Object.values(profile)[0]
+        jobTitle = Object.values(profile)[1]
+        company = Object.values(profile)[2]
+        numTabs = Object.values(profile)[3].length
+
+        //creates tabs 
+        for (var i = 0; i < numTabs; i++) {
+            title = Object.values(profile)[3][i].title
+            content = Object.values(profile)[3][i].content
+            tabTitle.push(<Tab style={ colors[i] }> {title} </Tab>);
+            tabContent.push(<TabPanel style={ colors[i] } className="tab-content"> {content} </TabPanel>);
+        }
 
         function nextProfile(curr) {
+            //var curr = 0;
             console.log("currProfile=" + curr)
-
+            console.log(allProfiles[1]);
             tabTitle = []
             tabContent = []
+            //this.forceUpdate();
 
+            //this doesn't do anything because it's rewritten 
             profile = allProfiles[curr];
             name = Object.values(profile)[0]
             jobTitle = Object.values(profile)[1]
@@ -93,7 +110,16 @@ export class StemProfessionals extends Component {
                 tabTitle.push(<Tab style={ colors[i] }> {title} </Tab>);
                 tabContent.push(<TabPanel style={ colors[i] } className="tab-content"> {content} </TabPanel>);
             }
+
+            // var element = (
+            //     <div className="profile">
+            //         <h4 className="name"> {name} </h4>
+            //         <h5 className="job-comp"> {jobTitle}  ●  {company} </h5>
+            //     </div>
+            // );
+            // ReactDOM.render(element, document.getElementsByClassName('profile'));
         }
+
 
         return (
             <div>
@@ -113,8 +139,7 @@ export class StemProfessionals extends Component {
                         </Tabs>
                     </div>
 
-                    <Button onClick={nextProfile(currProfile++)} className="button-right" icon="angle right"></Button>
-
+                    <Button onClick={()=>{ nextProfile(currProfile++); }} className="button-right" icon="angle right"></Button>
                 </div>
 
             </div>
